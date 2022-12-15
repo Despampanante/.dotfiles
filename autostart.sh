51e463949d25f042d3a79edb6b5cd4d4032f3eff
Santiago <santiago@depascale.org>
2022-12-15 18:26:24 -0500

    finished auto script

--- autostart.sh
+++ autostart.sh
@@ -1,17 +1,20 @@
-#find config/* -prune -exec \
-  #sh -c 'ln -s $(realpath {}) ~/.config/' \;
-#find home/.* -prune -type f -exec \
-  #sh -c 'ln -s $(realpath {}) ~' \;
-#echo Done!
-
-for file in config/*; do 
-  echo "$file"
-  if [ -d $file ]; then
-    echo "test"
-    echo $(basename $file)
-    echo "test"
+for file in config/* home/.*; do 
+  if [[ $file -ef . ]] || [[ $file -ef home/.  ]]; then  continue; fi
+  if [[ -d ~/.config/$(basename $file) ]] 
+  then
+    read -p "Delete ~/.config/$(basename $file)? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then echo " Deleting..."; rm -r ~/.config/$(basename $file); else echo; echo "Not removed"; fi
+  fi
+  if [[ -f ~/$(basename $file) ]] 
+  then
+    read -p "Delete ~/$(basename $file)? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then echo " Deleting..." ; rm ~/$(basename $file); else echo; echo "Not removed"; fi
   fi
 done
 
-
+find config/* -prune -exec \
+  sh -c 'ln -s -v $(realpath {}) ~/.config/' \;
+find home/.* -prune -type f -exec \
+  sh -c 'ln -s -v $(realpath {}) ~' \;
+echo Done!
 
