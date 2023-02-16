8d46b92584ca863cb91bbf845d9e3ceb31fc895b
Despampanante <santiago@depascale.org>
2023-02-16 13:37:37 -0500

    wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+return {
+	color_scheme = "tokyonight",
+	font = wezterm.font("Iosevka"),
+	font_size = 16.0,
+	window_background_opacity = 0.9,
+	hide_tab_bar_if_only_one_tab = true,
+}
