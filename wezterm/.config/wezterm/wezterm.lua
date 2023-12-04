5595da04f70ff0ebbcf5d081c8c30af06653ead8
Despampanante <santiago@depascale.org>
2023-12-04 02:10:12 -0500

    Good starting point

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,14 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 return {
 	color_scheme = "Dracula (Official)",
-	font = wezterm.font("Iosevka Nerd Font Mono"), 
-  font_size = 16.0,
+	font = wezterm.font("Iosevka Nerd Font Mono"),
+	font_size = 16.0,
 	window_background_opacity = 1,
-	}
+	window_decorations = "NONE",
+	enable_tab_bar = false,
+	keys = {
+		{ key = "F11", mods = "NONE", action = act.ToggleFullScreen },
+	},
+
+}
